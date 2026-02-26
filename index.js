0aa925ac88e4acd3fbe3684fe8f89c0b630b6459
2smarthrm <2smarthrm@gmail.com>
2026-02-26 12:18:11 +0000

    Update index.js

--- index.js
+++ index.js
@@ -717,7 +717,7 @@ app.post(
     try {
       await transporter.sendMail({
         from:'"Waveled" <no-reply@waveled.pt>',
-        to:["sales@waveled.com", "fabio.catela@exportech.com.pt", "kiosso.silva@exportech.com.pt"]  ,
+        to:["sales@waveled.com", "fabio.catela@exportech.com.pt"],
         subject: `Waveled • Novo pedido (${payload.tipo}) de ${payload.nome}`,
         html,
       });
